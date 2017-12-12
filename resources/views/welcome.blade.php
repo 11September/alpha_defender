4128c02907488bbcc84060d2a443a39175a04ea4
silver7784@gmail.com <Adgjmpt9>
2017-12-12 18:30:14 +0200

    section news and start poslugy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <div class="container">
             <div class="head-title">
                 <div class="num-block"></div>
-                <h2 class="title">НОВИНИ</h2>
+                <div class="title">НОВИНИ</div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
@@ -21,22 +21,64 @@
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1-desc">
                             <div class="news-title">
                             <div class="yel-block"></div>
-                            <h3 class="news-date">17.11.2017</h3>
-                            <p>Стислий опис новин або подій виконаний у  Arial Narrow висотою 14pt. без заголовку чи підзаголовку. </p>
+                                <div>
+                                    <h3 class="news-date">17.11.2017</h3>
+                                    <p>Стислий опис новин або подій виконаний у  Arial Narrow висотою 14pt. без заголовку чи підзаголовку. </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <div class="news"></div>
+                    <div class="news">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1">
+                            <img src="../images/default.png" alt="">
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1-desc">
+                            <div class="news-title">
+                                <div class="yel-block"></div>
+                                <div>
+                                    <h3 class="news-date">17.11.2017</h3>
+                                    <p>Стислий опис новин або подій виконаний у  Arial Narrow висотою 14pt. без заголовку чи підзаголовку. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <div class="news"></div>
+                    <div class="news">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1">
+                            <img src="../images/default.png" alt="">
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1-desc">
+                            <div class="news-title">
+                                <div class="yel-block"></div>
+                                <div>
+                                    <h3 class="news-date">17.11.2017</h3>
+                                    <p>Стислий опис новин або подій виконаний у  Arial Narrow висотою 14pt. без заголовку чи підзаголовку. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="all-news">
+                <div>ВСІ НОВИНИ</div>
+            </div>
+        </div>
+    </section>
+    <section class="poslugy">
+        <div class="container-fluid">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 nopad">
+                <div class="posluga-1">
+                    <div class="col-xs-5 col-sm-12 col-md-5 col-">
+
+                    </div>
+
                 </div>
             </div>
         </div>
     </section>
-
 @endsection
 
 @section('scripts')
