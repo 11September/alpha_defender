fa985b59c42ea3ae9cc3326931356d5f6eb9d6f3
silver7784@gmail.com <Adgjmpt9>
2017-12-12 17:20:21 +0200

    news 3 blocks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,30 @@
 
     <section class="intro-news">
         <div class="container">
+            <div class="head-title">
+                <div class="num-block"></div>
+                <h2 class="title">НОВИНИ</h2>
+            </div>
             <div class="row">
-                <div>
-                    <div class="num-block"></div>
-                    <h2 class="title">НОВИНИ</h2>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="news">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1">
+                            <img src="../images/default.png" alt="">
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 news-1-desc">
+                            <div class="news-title">
+                            <div class="yel-block"></div>
+                            <h3 class="news-date">17.11.2017</h3>
+                            <p>Стислий опис новин або подій виконаний у  Arial Narrow висотою 14pt. без заголовку чи підзаголовку. </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div col-xs-12 col-sm-12 col-md-3 col-lg-12>
-
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="news"></div>
                 </div>
-                <div col-xs-12 col-sm-12 col-md-3 col-lg-12>
-
-                </div>
-                <div col-xs-12 col-sm-12 col-md-3 col-lg-12>
-
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="news"></div>
                 </div>
             </div>
         </div>
