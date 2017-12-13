42a043332d8a466ad08d94da3bf37c597ad174dc
Stanislav <shiptya@icloud.com>
2017-12-13 16:10:40 +0200

    add more images and footer finally stage

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,8 +1,4 @@
 <link rel='stylesheet' href='{{ asset('css/bootstrap.min.css') }}' type='text/css' media='all'/>
-<<<<<<< HEAD
-<link rel='stylesheet' href='{{ asset('css/style.css') }}' type='text/css' media='all'/><link rel='stylesheet' href='{{ asset('css/style.css') }}' type='text/css' media='all'/>
-{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">--}}
-=======
 <link rel='stylesheet' href='{{ asset('css/style.css') }}' type='text/css' media='all'/>
->>>>>>> 4e86d8ea89df435e34b12954f6cc149e67b82841
+{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">--}}
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css'/>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,52 @@
-<footer class="container footer">
-    <div class="row footer-wrapper">
-        <div class="col-md-2">
-            <img class="footer-image" src="{{ asset('images/LOGO_small.png') }}" alt="">
-        </div>
-        <div class="col-md-8">
-            <p class="footer-description">
-                Замовити нас Ви можете день в день, та у вихідні дні,
-                здійснюємо послугу з інкасації та перевезення протягом години
-                з моменту отримання заявки.
-            </p>
-        </div>
-        <div class="col-md-2 text-center">
-            <ul class="list-inline">
-                <li class="list-inline-item"><a class="social-icon text-xs-center" target="_blank" href="#">FB</a></li>
-                <li class="list-inline-item"><a class="social-icon text-xs-center" target="_blank" href="#">G+</a></li>
-                <li class="list-inline-item"><a class="social-icon text-xs-center" target="_blank" href="#">T</li>
-            </ul>
+<footer class="footer">
+    <div class="footer-background">
+        <div class="container">
+            <div class="row footer-wrapper">
+                <div class="col-md-3">
+                    <img class="footer-image" src="{{ asset('images/LOGO_small.png') }}" alt="">
+                </div>
+                <div class="col-md-6">
+                    <div class="wrrapper-description">
+                        <p class="footer-description-text">
+                            Замовити нас Ви можете день в день, та у вихідні дні, <br>
+                            здійснюємо послугу з інкасації та перевезення протягом години <br>
+                            з моменту отримання заявки.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-3 text-center">
+                    <div class="social-wrapper">
+                        <div class="social-block">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-youtube fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-rss fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
     <div class="row copyright">
-        <p class="copyright-text">АльфаЗахист  &#9400;  2017</p>
+        <p class="copyright-text">АльфаЗахист &#9400; 2017</p>
     </div>
 </footer>
