ab99e967ca14ed1888e42c78e087b4fed70d8a49
Stanislav <shiptya@icloud.com>
2017-12-16 11:20:41 +0200

    some fix pagination and search field and a:hovers elements

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -186,13 +186,6 @@
 
                     {{ $posts->links() }}
 
-                    {{--<ul class="pagination">--}}
-                        {{--<li class="page-item"><a class="page-link" href="#">Previous</a></li>--}}
-                        {{--<li class="page-item"><a class="page-link" href="#">1</a></li>--}}
-                        {{--<li class="page-item"><a class="page-link" href="#">2</a></li>--}}
-                        {{--<li class="page-item"><a class="page-link" href="#">3</a></li>--}}
-                        {{--<li class="page-item"><a class="page-link" href="#">Next</a></li>--}}
-                    {{--</ul>--}}
                 </div>
             </div>
 
