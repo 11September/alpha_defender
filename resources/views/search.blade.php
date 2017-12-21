3183ecc41ed17cdd7f0fdcac8170b0f49c97c1cf
Stanislav <shiptya@icloud.com>
2017-12-21 16:08:27 +0200

    last 20 changes (light search text)

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -30,7 +30,7 @@
                 </div>
             @endif
 
-            @include('partials.posts-items')
+            @include('partials.posts-items-search')
 
             <div class="row">
                 <div class="col-lg-6 offset-lg-3 py-5">
