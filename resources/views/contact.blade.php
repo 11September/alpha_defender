59bf5849cc0a52ef021d104efe8d10eb55d1853b
Stanislav Shiptya <xxxsp1nxxx@mail.ru>
2017-12-26 20:37:13 +0200

    20 edits end captcha

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,11 +21,13 @@
                     <div class="col-md-6 col-xl-4">
                         <div class="wrapper-contact-item">
                             <div class="wrapper-image-contacts">
-                                <img class="avatar" src="{{ asset('storage/' . $person->image) }}" alt="{{ $person->name }}">
+                                <img class="avatar" src="{{ asset('storage/' . $person->image) }}"
+                                     alt="{{ $person->name }}">
                             </div>
                             <div class="wrapper-contact-info">
                                 <p class="position">{{ $person->position }}</p>
-                                <p class="email"><a href="mailto:{{ $person->email }}?Subject=Hello%20again" target="_top">{{ $person->email }}</a>
+                                <p class="email"><a href="mailto:{{ $person->email }}?Subject=Hello%20again"
+                                                    target="_top">{{ $person->email }}</a>
                                 </p>
                                 <p class="phone"><a href="tel:{{ $person->phone }}">{{ $person->phone }}</a></p>
                             </div>
@@ -36,7 +38,8 @@
                 <div class="row">
                     <div class="col-md-12">
                         <p class="contact-intro-text">
-                            Запрошуємо Вас завітати до нашого головного офісу за адресою: Київська область, м. Бориспіль,
+                            Запрошуємо Вас завітати до нашого головного офісу за адресою: Київська область, м.
+                            Бориспіль,
                             вул. Запорізька, 14.</p>
                     </div>
                 </div>
@@ -62,10 +65,12 @@
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                            <input value="{{ old('name') }}" name="name" type="text" required="required" class="form-control" placeholder="Як до Вас звертатись? ...">
+                            <input value="{{ old('name') }}" name="name" type="text" required="required"
+                                   class="form-control" placeholder="Як до Вас звертатись? ...">
                         </div>
                         <div class="form-group">
-                            <input value="{{ old('email') }}" name="email" type="email" required="required" class="form-control" placeholder="e-mail,  для зворотнього зв’язку ...">
+                            <input value="{{ old('email') }}" name="email" type="email" required="required"
+                                   class="form-control" placeholder="e-mail,  для зворотнього зв’язку ...">
                         </div>
                         <div class="form-group">
                             <textarea name="message" class="form-control" rows="8" required="required"
@@ -73,7 +78,11 @@
                         </div>
 
                         <div class="form-group">
-                            {!! NoCaptcha::display(['data-theme' => 'light']) !!}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    {!! NoCaptcha::display(['data-theme' => 'light', 'data-size' => 'normal']) !!}
+                                </div>
+                            </div>
                         </div>
 
                         <div class="col-md-12 wrapper-submit-button">
@@ -97,7 +106,8 @@
                                 дізнатися про наші послуги.
                             </li>
                             <li class="contact-list">Для замовлення послуги скористайтеся функцією
-                                <a class="order-now" data-toggle="modal" data-target="#myModal" href="#reviews">“ЗАМОВИТИ ЗАРАЗ”</a>
+                                <a class="order-now" data-toggle="modal" data-target="#myModal" href="#reviews">“ЗАМОВИТИ
+                                    ЗАРАЗ”</a>
                             </li>
                         </ul>
                     </div>
